875a8bdb84923c8283baef38ddf8549d9a2f0b19
Josep Crego <jmcrego@gmail.com>
2026-03-30 14:47:31 +0200

    debug

--- run_job.sh
+++ run_job.sh
@@ -29,4 +29,6 @@ TSV=resources/UD-enfr.tsv
 
 #python udinflector.py resources/UD-enfr.tsv --model $MODEL --lang English
 #python udinflector.py resources/UD-fren.tsv --model $MODEL --lang French
-PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python conjugator.py --language French --term chanter --pos verb
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python conjugator.py --tsv $TSV --model $MODEL --language English
+
+
