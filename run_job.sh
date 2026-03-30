3fb9687602ba964677127a0d967180f71fd59bf1
Josep Crego <jmcrego@gmail.com>
2026-03-30 11:47:04 +0200

    light

--- run_job.sh
+++ run_job.sh
@@ -1,24 +1,24 @@
-#!/bin/bash                                                                                                                                                                                                                                             
+#!/bin/bash
 
-#SBATCH --job-name=udinflector                                                                                                                                                                                                                          
-#SBATCH --output=./logs/%u.%x.%j.out                                                                                                                                                                                                                    
-#SBATCH --error=./logs/%u.%x.%j.log                                                                                                                                                                                                                     
-#SBATCH --gres=gpu:1                                                                                                                                                                                                                                    
-#SBATCH --nodes=1                                                                                                                                                                                                                                       
-#SBATCH --cpus-per-task=10                                                                                                                                                                                                                              
-#SBATCH --time=8:00:00                                                                                                                                                                                                                                  
+#SBATCH --job-name=udinflector
+#SBATCH --output=./logs/%u.%x.%j.out
+#SBATCH --error=./logs/%u.%x.%j.log
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=8:00:00
 
-### uncomment one of the next ###                                                                                                                                                                                                                       
-#                                                                                                                                                                                                                                                       
-##SBATCH -C h100                                                                                                                                                                                                                                         
-##SBATCH -A eut@h100                                                                                                                                                                                                                                     
-#                                                                                                                                                                                                                                                       
-#SBATCH -C v100-32g                                                                                                                                                                                                                                    
-#SBATCH -A eut@v100                                                                                                                                                                                                                                    
+### uncomment one of the next ### 
+#
+##SBATCH -C h100
+##SBATCH -A eut@h100
+#
+#SBATCH -C v100-32g
+#SBATCH -A eut@v100
 
 
-#copy data from macbook:                                                                                                                                                                                                                                
-#scp -v -3 UD-enfr.tsv ujt99zo@jean-zay.idris.fr:/linkhome/rech/genata01/ujt99zo/work/josep/UdInflector/                                                                                                                                                
+#copy data from macbook:
+#scp -v -3 UD-enfr.tsv ujt99zo@jean-zay.idris.fr:/linkhome/rech/genata01/ujt99zo/work/josep/UdInflector/
 
 module purge
 module load arch/h100 #this module must be loaded before pytorch-gpu                                                                                                                                                                                    
@@ -27,5 +27,5 @@ module load pytorch-gpu/py3/2.6.0
 #MODEL=/lustre/fsmisc/dataset/HuggingFace_Models/Qwen/Qwen3-32B
 MODEL=/lustre/fsmisc/dataset/HuggingFace_Models/Qwen/Qwen3-8B
 
-#python udinflector.py resources/UD-enfr.tsv --model $MODEL --lang English                                                                                                                                                                              
-#python udinflector.py resources/UD-fren.tsv --model $MODEL --lang French                
+#python udinflector.py resources/UD-enfr.tsv --model $MODEL --lang English
+#python udinflector.py resources/UD-fren.tsv --model $MODEL --lang French
