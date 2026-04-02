5d69b9ac9711c791ef3790a6de9b166e4a9c1dc3
Josep Crego <jmcrego@gmail.com>
2026-04-02 14:22:20 +0200

    light

--- run_job.sh
+++ run_job.sh
@@ -19,9 +19,6 @@
 #copy data from macbook:
 #scp -v -3 ud-enfr_{en,fr}.{dic,xml} ujt99zo@jean-zay.idris.fr:/linkhome/rech/genata01/ujt99zo/work/josep/UdInflector/
 
-# python fix_xml.py < resources/ud-enfr_fr.xml > resources/ud-enfr_fr.fixed.xml   
-# python fix_xml.py < resources/ud-enfr_en.xml > resources/ud-enfr_en.fixed.xml 
-
 # Build glossaries with entries : POS \t term_src \t term_tgt
 # python SystranUD2glossary.py resources/ud-enfr_fr.dic resources/ud-enfr_en.dic --oname resources/ud --lang1 fr --lang2 en
 
@@ -30,13 +27,16 @@ module purge
 module load arch/h100 #this module must be loaded before pytorch-gpu                                                                                                                                                                                    
 module load pytorch-gpu/py3/2.6.0
 
-MODEL=/lustre/fsmisc/dataset/HuggingFace_Models/Qwen/Qwen3-32B #Qwen3-32B Qwen3-8B
+MODEL=/lustre/fsmisc/dataset/HuggingFace_Models/Qwen/Qwen3-8B #Qwen3-32B Qwen3-8B
+NAME=Qwen3-8B
 
 #
-python inflect.py resources/ud-fren.tsv --model $MODEL --language French
+python inflect.py resources/ud-fren.tsv --model $MODEL --language French --out resources/ud-fren.tsv.$NAME.inflections.tsv
 #
-python inflect.py resources/ud-enfr.tsv --model $MODEL --language English
+python inflect.py resources/ud-enfr.tsv --model $MODEL --language English --out resources/ud-enfr.tsv.$NAME.inflections.tsv
 
 # Evaluation
-# python eval_inflector.py resources/ud-enfr_en.fixed.xml resources/ud-enfr.tsv.inflections.tsv --verbose > resources/ud-enfr.tsv.inflections.tsv.eval
-# python eval_inflector.py resources/ud-enfr_fr.fixed.xml resources/ud-enfr.tsv.inflections.tsv --verbose > resources/ud-enfr.tsv.inflections.tsv.eval
+# 
+python eval_inflector.py resources/ud-enfr_en.fixed.xml resources/ud-enfr.tsv.$NAME.inflections.tsv --verbose > resources/ud-enfr.tsv.$NAME.inflections.tsv.eval
+# 
+python eval_inflector.py resources/ud-enfr_fr.fixed.xml resources/ud-enfr.tsv.$NAME.inflections.tsv --verbose > resources/ud-enfr.tsv.$NAME.inflections.tsv.eval
