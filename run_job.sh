115788ee8ad6afa0da5f24f36877f4e1033ab11e
Josep Crego <jmcrego@gmail.com>
2026-04-10 10:27:33 +0200

    light

--- run_job.sh
+++ run_job.sh
@@ -42,8 +42,10 @@ inflection(){
     python evaluator.py $refs $dict.$NAME.tsv --verbose > $dict.$NAME.tsv.eval
 }
 
-#inflection resources/ud-fren.tsv French resources/ud-enfr_fr.xml
-#inflection resources/ud-enfr.tsv English resources/ud-enfr_en.xml
+#
+inflection resources/ud-fren.tsv French resources/ud-enfr_fr.xml
+#
+inflection resources/ud-enfr.tsv English resources/ud-enfr_en.xml
 
 
 # python grammarbook.py resources/Grammaire.pdf
