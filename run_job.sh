4ac3f742704f0c683e7ff0161f26e54c524d011d
Josep Crego <jmcrego@gmail.com>
2026-04-10 22:03:08 +0200

    debug

--- run_job.sh
+++ run_job.sh
@@ -42,9 +42,10 @@ inflection(){
     # python evaluator.py $refs $dict.$NAME.tsv --verbose > $dict.$NAME.tsv.eval
 }
 
-inflection difficult.tsv French resources/ud-enfr_fr.xml
+#inflection difficult.tsv French resources/ud-enfr_fr.xml
 
-#inflection resources/ud-fren.tsv French resources/ud-enfr_fr.xml
+#
+inflection resources/ud-fren.tsv French resources/ud-enfr_fr.xml
 #inflection resources/ud-enfr.tsv English resources/ud-enfr_en.xml
 
 
